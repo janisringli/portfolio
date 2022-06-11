5fd216ba9f46eda643a760acc9bc6dc9f33079b0
“Janis <janis.ringli@gmail.com>
2022-06-11 14:36:54 +0200

    implement circle progress

--- src/age.js
+++
@@ -1,6 +0,0 @@
-let currentDate = new Date();
-let birthDate = new Date("2004/07/03");
-let difference = currentDate - birthDate;
-let age = Math.floor(difference / 31557600000);
-console.log(age);
-document.getElementById("age").innerHTML = age;
